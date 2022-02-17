eb4e3951cac4d3ae2106cb7fb0fe43bd8e9d79f1
sleepykanguru22 <70289696+sleepykanguru22@users.noreply.github.com>
2022-02-17 14:35:26 -0800

    Update Photos.js

--- Photos.js
+++ Photos.js
@@ -13,7 +13,7 @@ export default function Photos(props) {
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"
-                    alt="search results"
+                    alt="results"
                   />
                 </a>
               </div>
